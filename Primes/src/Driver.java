ccb5dbf205abf858625733cbba3036901aacf6b2
Happy <Happy@HappyKimoPavilion.attlocal.net>
2021-02-10 11:17:17 -0800

    compute doesnt return anything anymore and no more arraylist

--- Primes/src/Driver.java
+++ Primes/src/Driver.java
@@ -4,19 +4,19 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int maxNum = 100000;						//max limit
+		int maxNum = 1000000;						//max limit
 		int[] nums = new int[maxNum];
 		for(int i =0; i < nums.length;i++) {	//initialize the integer array
 			nums[i]=i+1;
 		}
-		 //compute the primes
-		display(compute(nums)); //display the primes
+		compute(nums); //compute the primes
+		display(nums); //display the primes
 	}
 	
 	/* Use the algorithm described in the lab description so that the array, nums, contains all the
 	 * valid primes. All others should be -1. 
 	 */
-	public static ArrayList<Integer> compute(int[]nums) {
+	public static void compute(int[]nums) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		for(int i=2;i<=nums.length;i++) {
 			for(int j=2;i<=i;j++) {
@@ -29,14 +29,13 @@ public class Driver {
 			}
 			
 		}
-		return result;
 	}
 	/* 
 	 * Complete the method below so that it prints all the prime numbers in the integer array nums
 	 * Assume that the compute method has been called that sets the array to contain the prime numbers as 
 	 * described in the lab document. 
 	 */
-	 public static void display(ArrayList<Integer> list) {
+	 public static void display(int[] list) {
 
 	        for(int integer: list) {
 	            System.out.println(integer + " ");
